36631b5db66300c26fe59dc6e9ff8aa973870fb1
oakkar-inc <oakkarphyosp@gmail.com>
2025-02-27 15:34:44 +0800

    Add mobile nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -11,13 +11,13 @@ export function MainNav() {
 
   return (
     <nav className="flex items-center space-x-4 lg:space-x-6">
-      <Link href="/" className="mr-6 flex items-center spacce-x-2">
-        <span className="ml-1 font-bold">{siteConfig.name}</span>
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <span className="font-bold">{siteConfig.name}</span>
       </Link>
       <Link
         href="/blog"
         className={cn(
-          "text-sm font-medium trasition-colors hover:text-primary",
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
           pathName === "/blog" ? "text-foreground" : "text-foreground/60"
         )}
       >
@@ -26,7 +26,7 @@ export function MainNav() {
       <Link
         href="/about"
         className={cn(
-          "text-sm font-medium trasition-colors hover:text-primary",
+          "text-sm font-medium trasition-colors hover:text-primary hidden sm:inline-block",
           pathName === "/about" ? "text-foreground" : "text-foreground/60"
         )}
       >
