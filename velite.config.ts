3dbba66975cfaedf868e3df71fd7c1a427dac97e
oakkar-inc <oakkarphyosp@gmail.com>
2025-02-28 20:23:16 +0800

    Set up code block styling

--- velite.config.ts
+++ velite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig, defineCollection, s} from "velite"
+import { defineConfig, defineCollection, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-const computedFields = <T extends { slug: string}> (data: T) => ({
+const computedFields = <T extends { slug: string }>(data: T) => ({
   ...data,
   slugAsParams: data.slug.split("/").slice(1).join("/"),
-})
-
+});
 
 const posts = defineCollection({
   name: "Post",
   pattern: "blog/**/*.mdx",
-  schema: s.object({
-    slug: s.path(),
-    title: s.string().max(99),
-    description: s.string().max(999).optional(),
-    date: s.isodate(),
-    published: s.boolean().default(true),
-    body: s.mdx(),
-  })
-  .transform(computedFields),
-})
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
 
 export default defineConfig({
   root: "content",
@@ -31,7 +34,20 @@ export default defineConfig({
   },
   collections: { posts },
   mdx: {
-    rehypePlugins: [],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
     remarkPlugins: [],
   },
-})
+});
