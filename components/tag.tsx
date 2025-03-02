34a5d310e11800746acf6ebf6bf851b25edfe23c
oakkar-inc <oakkarphyosp@gmail.com>
2025-03-03 03:40:01 +0800

    Add tags card

--- components/tag.tsx
+++ components/tag.tsx
@@ -7,7 +7,7 @@ interface TagProps {
   current?: boolean;
   count?: number;
 }
-export function Tag({ tag, current }: TagProps) {
+export function Tag({ tag, current, count }: TagProps) {
   return (
     <Link
       className={badgeVariants({
@@ -16,7 +16,7 @@ export function Tag({ tag, current }: TagProps) {
       })}
       href={`/tags/${slug(tag)}`}
     >
-      {tag}
+      {tag} {count ? `(${count})` : null}
     </Link>
   );
 }
