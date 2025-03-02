cc296d2ef18a24677fff5d4b6843f19e6170e218
oakkar-inc <oakkarphyosp@gmail.com>
2025-03-03 04:30:08 +0800

    Fix bug for post links not working from tags

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,20 +1,29 @@
+"use client";
+
 import { slug } from "github-slugger";
 import Link from "next/link";
 import { badgeVariants } from "./ui/badge";
+import { useSearchParams } from "next/navigation";
 
 interface TagProps {
   tag: string;
   current?: boolean;
   count?: number;
 }
+
 export function Tag({ tag, current, count }: TagProps) {
+  const searchParams = useSearchParams();
+  const currentLang = searchParams.get("lang") || "en";
+
+  const tagUrl = `/tags/${slug(tag)}?lang=${currentLang}`;
+  
   return (
     <Link
       className={badgeVariants({
         variant: current ? "default" : "secondary",
         className: "no-underline rounded-md",
       })}
-      href={`/tags/${slug(tag)}`}
+      href={tagUrl}
     >
       {tag} {count ? `(${count})` : null}
     </Link>
